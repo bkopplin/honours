74610c18b734c13554bc5dc8c10d9b0bd38a9693
bkopplin <bjarne.kopplin@pm.me>
2022-02-07 17:06:20 +0000

    eunit tests for match_col_row()

--- src/db.erl
+++ src/db.erl
@@ -4,6 +4,7 @@
 -export([connect/0]).
 
 -include_lib("epgsql/include/epgsql.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 %% Connects to the local database
 %% Connection information is hardcoded for the time being
@@ -35,15 +36,14 @@ get_event(RoomId, EventId) ->
 %% Returns a list of messages in room RoomId with filter parameters Qs
 get_messages(RoomId, Qs) ->
 	C = connect(),
-	Order = case maps:get(dir, Qs) of <<"f">> -> "ASC"; _ -> "DESC" end,
 	Limit = maps:get(limit, Qs),
 	case epgsql:equery(C, "SELECT content, event_id, origin_server_ts, room_id, sender, type, unsigned, state_key FROM Events WHERE room_id = $1 ORDER BY depth DESC LIMIT $2", [RoomId, Limit]) of
+		{ok, _Cols, []} ->
+			ok = epgsql:close(C),
+			{error, not_found};
 		{ok, Cols, Rows} ->
 			ok = epgsql:close(C),
 			{ok,table_to_list(Cols, Rows)};
-		{ok, _, []} ->
-			ok = epgsql:close(C),
-			{error, not_found};
 		{error, Reason} ->
 			ok = epgsql:close(C),
 			{error, Reason}
@@ -51,17 +51,61 @@ get_messages(RoomId, Qs) ->
 
 table_to_list(Cols, [Row|Rest]) ->
 	[maps:from_list(match_col_row(Cols, erlang:tuple_to_list(Row)))|table_to_list(Cols, Rest)];
-table_to_list(Cols, []) -> [].
+table_to_list(_Cols, []) -> [].
 
-%% Converts a list of column tuples and a list of row contents to a key-value map that maps a column name to a row cell
+%% 
+%% Converts the given list of epgsql-columns and a corresponding list of row elements into a list of the form [{columnName, RowElement}].
+%% Note that both the column and row have to be of type list.
+%%
 match_col_row([Col|RemCols], [RowElement|RemRow]) ->
 	ColTitle = Col#column.name,
 	case Col#column.type of
 		jsonb ->
 			[{ColTitle, jiffy:decode(RowElement, [return_maps])}|match_col_row(RemCols, RemRow)];
+		int8 ->
+			[{ColTitle, erlang:binary_to_integer(RowElement)}|match_col_row(RemCols, RemRow)];
 		_ ->
 			[{ColTitle, RowElement}|match_col_row(RemCols, RemRow)]
 	end;
 match_col_row([], []) ->
 	[].
 
+-ifdef(EUNIT).
+%%% -----
+%%% Tests
+%%% -----
+match_col_row_test() ->
+	?assertEqual([], match_col_row([],[])).
+
+match_col_row_single_column_test() ->
+	?assertEqual([{<<"event_id">>, <<"123">>}], match_col_row([generate_column(<<"event_id">>, text)], [<<"123">>]) ).
+
+match_col_row_two_columns_test() ->
+	?assertEqual(
+	   [{<<"content">>, #{<<"body">> => <<"hello world">>, <<"msgtype">> => <<"m.text">>}},
+	    {<<"event_id">>, <<"123">>},
+	    {<<"origin_server_ts">>, 1638547064954}], 
+	   match_col_row(
+	     [generate_column(<<"content">>, jsonb),
+	      generate_column(<<"event_id">>, text),
+	      generate_column(<<"origin_server_ts">>, int8)
+	     ], 
+	     [
+	 	<<"{\"body\": \"hello world\", \"msgtype\": \"m.text\"}">>, <<"123">>,<<"1638547064954">>
+	     ])).
+
+
+%%% -------------------------
+%%% Internal Helper Functions
+%%% -------------------------
+
+%%%
+%%% Generates an epgsql column for testing purposes
+%%% Name :: binary string
+%%% type :: atom
+%%%
+generate_column(Name, Type) ->
+	#column{name = Name,type = Type,oid = 3802,
+        size = -1,modifier = -1,format = 0,table_oid = 16619,
+        table_attr_number = 1}.
+-endif.

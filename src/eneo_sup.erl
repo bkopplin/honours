be2b019c052d92978b76c623de46353f3846fb7a
bkopplin <bjarne.kopplin@pm.me>
2022-03-02 16:34:21 +0000

    environment variables within docker-compose

--- src/eneo_sup.erl
+++ src/eneo_sup.erl
@@ -41,12 +41,37 @@ children() ->
 		%				 database => "bjarne",
 		%				 timeout => 4000
 		%				},
-		{ok, DbConfig} = application:get_env(eneo, dbconfig),
+		%{ok, DbConfig} = application:get_env(eneo, dbconfig),
 		[#{id => db,
-		   start => {db, start_link, [DbConfig]},
+		   start => {db, start_link, [dbconfig()]},
 		   restart => permanent,
 		   shutdown => brutal_kill,
 		   type => worker,
 		   modules => [db]
 		  }].
 
+dbconfig() ->
+	Host = case os:getenv("PG_HOST") of
+		false -> "localhost";
+		[] -> "localhost";
+		PGHOST -> PGHOST end,
+	Username = case os:getenv("PG_USER") of
+		false -> "eneo";
+		[] -> "eneo";
+		PGUSER -> PGUSER 
+		end,
+	Password = case os:getenv("PG_PASSWORD") of
+		false -> "";
+		[] -> "";
+		PGPW -> PGPW
+		end,
+	Database = case os:getenv("PG_DATABASE") of
+		false -> "eneo";
+		[] -> "eneo";
+		PGDB -> PGDB
+		end,
+	 #{host => Host,
+		username => Username,
+		password => Password,
+		database => Database,
+		timeout => 4000}.

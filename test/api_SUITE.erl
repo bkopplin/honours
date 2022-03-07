1e86a12642522bb4d0099ebc61f7274366bd2a46
bkopplin <bjarne.kopplin@pm.me>
2022-03-07 12:27:44 +0000

    setup database in ct

--- test/api_SUITE.erl
+++ test/api_SUITE.erl
@@ -8,41 +8,52 @@ suite() ->
 		[{timetrap,{seconds,20}}].
 
 init_per_suite(Config) ->
-		os:putenv("PG_PASSWORD", "password"),
-		os:cmd("../../../default/rel/eneo/bin/eneo daemon"),
-		ibrowse:start(),
-		Config.
+	Database = "eneo-test",
+	Root = "../../../..",
+	os:cmd("createdb " ++ Database),
+	os:cmd("psql -d " ++ Database ++ " -f " ++ Root ++ "/sql/init.sql"),
+	% TODO find better solution than using env vars, e.g. config files 
+	os:putenv("PG_DATABASE", Database),
+	os:putenv("PG_USER", "bjarne"),
+	os:cmd(Root ++ "/_build/default/rel/eneo/bin/eneo daemon"),
+	ibrowse:start(),
+	[{db, Database}|Config].
 
-end_per_suite(_Config) ->
-		os:cmd("../../../default/rel/eneo/bin/eneo stop"),
-		ok.
+end_per_suite(Config) ->
+	os:cmd("dropdb " ++ proplists:get_value(db, Config)),
+	os:cmd("../../../default/rel/eneo/bin/eneo stop"),
+	ok.
 
 init_per_group(_GroupName, Config) ->
-		Config.
+	Config.
 
 end_per_group(_GroupName, _Config) ->
-		ok.
+	ok.
 
 init_per_testcase(_TestCase, Config) ->
-		Config.
+	Config.
 
 end_per_testcase(_TestCase, _Config) ->
-		ok.
+	ok.
 
 groups() ->
-		[].
+	[].
 
 all() ->
-		[app_running_case].
+	[app_running_case].
 
 app_running_case(Config) ->
-		{ok, "200", _, Body} = ibrowse:send_req("http://127.0.0.1:8080/rooms/!r1:localhost/messages", [], get),
-		ct:pal(Body),
-		ok.
+	{ok, "200", _, Body} = ibrowse:send_req("http://127.0.0.1:8080/rooms/!r1:localhost/messages", [], get),
+	ct:pal(Body),
+	ok.
 
 rooms_messages_case(Config) ->
 	ct:pal("~p", [application:info()]),
 	ct:pal("eneo_sup: ~p", [whereis(eneo_sup)]),
 	{ok, "200", _, Body} = ibrowse:send_req("http://localhost:8080/rooms/!r1:localhost/messages", [], get),
 	ok.
+
+%%% ------------------------
+%%% Internal Helper Functions
+%%% -------------------------
 

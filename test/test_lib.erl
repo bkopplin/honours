a316a84f36c45bfb9326285f2d13ea4363437c2f
bkopplin <bjarne.kopplin@pm.me>
2022-03-14 09:09:53 +0000

    fixed init mock db.

--- test/test_lib.erl
+++ test/test_lib.erl
@@ -1,15 +1,19 @@
 -module(test_lib).
 
--export([mock_db/0, insert_from_file/3]).
+-export([db/0, init_db/1, insert_from_file/3]).
 
-mock_db() ->
+db() ->
 		{ok, C} = epgsql:connect(#{host => "localhost",
 								  username => "bjarne",
 								  password => "password",
 								  database => "eneo-test"}),
-		 {ok, Sql} = file:read_file(code:priv_dir(eneo) ++ "/sql/init.sql"),
-		 epgsql:squery(C, Sql),
 		 {ok, C}.
+
+init_db(C) ->
+	{ok, Sql} = file:read_file(code:priv_dir(eneo) ++ "/sql/init.sql"),
+	epgsql:squery(C, Sql).
+
+
 
 insert_from_file(C, Application, Filename) ->
 		Filepath = code:priv_dir(Application) ++ Filename,

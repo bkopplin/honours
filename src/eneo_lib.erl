c7e7861545997ef9f04a4c6ec1e9361897b1cdf5
bkopplin <bjarne.kopplin@pm.me>
2022-03-29 17:53:53 +0100

    using cowboy_rest behaviour for whoami endpoint

--- src/eneo_lib.erl
+++ src/eneo_lib.erl
@@ -12,16 +12,20 @@ gen_state_id() ->
 	Id = base64:encode(crypto:strong_rand_bytes(20)),
 	<<"$",Id/binary >>.
 
+%% @doc Checks if a given user token is valid and returns the corresponding user_id or error
+%% code, as the case may be.
+-spec authenticate_token(cowboy:req()) -> {ok, binary()} | {error, integer(), atom(), binary()}.
 authenticate_token(Req) ->
 	case parse_token(Req) of
-		{error, Type, Error} ->
-			{error, Type, Error};
+		{error, Status, Type, Error} ->
+			{error, Status, Type, Error};
 		{ok, Token} ->
+			io:format("token ~p~n", [Token]),
 			case db:validate_token(Token) of
 				{ok, UserId} ->
-					{ok, Req#{user_id => UserId}};
+					{ok, UserId};
 				{error, invalid_token} ->
-					{error, invalid_token, <<"Invalid token specified">>}
+					{error, 401, m_invalid_token, <<"Invalid token specified">>}
 			end
 	end.
 
@@ -31,16 +35,16 @@ parse_token(Req) ->
 	of
 		undefined ->
 			case cowboy_req:match_qs([{access_token, nonempty, undefined}], Req) of
+				#{access_token := undefined} -> 
+					{error, 401, m_missing_token, <<"Missing access token">>};
 				#{access_token := Tk1} ->
-					{ok, Tk1};
-				undefined -> 
-					{error, missing_token, <<"Missing access token">>}
+					{ok, Tk1}
 			end;
 		{bearer, <<_:0/binary>>} ->
-			{error, missing_token, <<"Missing access token">>};
+			{error, 401, m_missing_token, <<"Missing access token">>};
 		{bearer, Tk2} ->
 			{ok, Tk2}
 	catch
 		exit:{request_error, _,_}:_ ->
-			{error, invalid_header, <<"Invalid Authorization header">>}
+			{error, 401, m_invalid_header, <<"Invalid Authorization header">>} %TODO check status code
 	end.

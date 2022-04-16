596b83ace90e671e177c47ec7b8df20d2cf1edd0
bkopplin <bjarne.kopplin@pm.me>
2022-04-16 11:51:52 +0100

    setup loadtesting locally

--- test/cluster/loadtests/get_messages.js
+++ test/cluster/loadtests/get_messages.js
@@ -2,16 +2,18 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 const host = 'http://127.23.0.1:8081';
-//const host = 'http://docker_loadbalancer_1';
 
 export const options = {
 		stages: [
-				{duration: '10s', target: 20},
+				{duration: '60s', target: 20},
 		]
 };
 
 export default function() {
 		const res = http.get(host + '/rooms/!r1:localhost/messages');
-		check(res, { 'status was 200': (r) => r.status == 200});
+		check(res, { 
+				'status was 200': (r) => r.status == 200,
+				'number of events was 3': (res) => res.json().length == 3, 
+		});
 		sleep(0.0001);
 }

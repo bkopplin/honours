8e672ce2f7b6b9c72b012e2f0d769c1a3e3a0691
bkopplin <bjarne.kopplin@pm.me>
2022-04-18 10:44:00 +0100

    build Docker image automatically and architecture independent

--- test/performance-tests/run-test.sh
+++ test/performance-tests/run-test.sh
@@ -1,10 +1,10 @@
 if [  $# != 3 ]; then
 	echo "Runs load tests and saves them to the correct path"
 	echo "${0} <dir> <filename> <test-script>"
+	exit 1
 fi
 OUTDIR=$1
 FILE=$2
 SCRIPT=$3
 
-k6 run $SCRIPT | tee /dev/tty | > ${OUTDIR}/full/${FILE}.txt |  grep -e checks -e http_req_duration > ${OUTDIR}/filtered/${FILE}.txt
-
+k6 run --env HOST='127.0.0.1:8081' $SCRIPT | tee /dev/tty | tee ${OUTDIR}/${FILE}.txt
---
+++ test/performance-tests/shadow_test.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+		stages: [
+				{duration: '1s', target: 1},
+		]
+};
+
+export default function() {
+		const res = http.get('http://' + __ENV.HOST + '/rooms/!r1:localhost/messages');
+		check(res, { 
+				'status was 200': (r) => r.status == 200,
+				'number of events was 3': (res) => res.json().length == 3, 
+		});
+		sleep(0.0001);
+}

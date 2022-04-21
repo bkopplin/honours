9e9f67a543db2b103b75867fa7fdf5dd4453c921
bkopplin <bjarne.kopplin@pm.me>
2022-04-21 10:10:27 +0100

    performance tests

---
+++ test/performance-tests/performance.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+		stages: [
+				{duration: '1m', target: 200},
+				{duration: '2m', target: 100},
+				{duration: '1m', target: 0},
+		]
+};
+
+export default function() {
+		const res = http.get('http://' + __ENV.HOST + '/rooms/!r0:localhost/messages');
+		check(res, { 
+				'status was 200': (r) => r.status == 200,
+				'number of events was 3': (res) => res.json().length == 3, 
+		});
+		sleep(0.0001);
+}
---
+++ test/performance-tests/performance_3.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+		stages: [
+				{duration: '4m', target: 600},
+				{duration: '5m', target: 600},
+				{duration: '1m', target: 0},
+		]
+};
+
+export default function() {
+		const res = http.get('http://' + __ENV.HOST + '/rooms/!r0:localhost/messages');
+		console.log("body:" , res.body);
+		console.log("code:" , res.status);
+		check(res, { 
+				'status was 200': (r) => r.status == 200,
+				'number of events was 3': (res) => res.json().length == 3, 
+		});
+		sleep(0.0001);
+}
